a8a7653bc4e162b5f2751a7fc99f6d82f540bbe2
Hesham\Hanafawy <hesham.hanafi911@gmail.com>
2019-10-12 13:16:52 +0200

    add route of event  trainer and workshops

--- src/routes/web.php
+++ src/routes/web.php
@@ -13,7 +13,44 @@ Route::group(
         'prefix'    => 'admin'
     ], function (){
 
-        Route::get('index', 'EventsController@index');
+        /*  Event Route */
+        Route::group([
+            'prefix'    => 'events'
+        ],function(){
+            Route::get('/', 'EventsController@index');
+            Route::get('event/{id}', 'EventsController@show');
+            Route::get('event/create', 'EventsController@create');
+            Route::post('event/create', 'EventsController@store');
+            Route::get('event/edit/{id}', 'EventsController@edit');
+            Route::post('event/edit/{id}', 'EventsController@update');
+            Route::post('event/delete/{id}', 'EventsController@delete');
+        });
+
+    /*  Trainer Route */
+    Route::group([
+        'prefix'    => 'trainers'
+    ],function(){
+        Route::get('/', 'EventsController@index');
+        Route::get('trainer/{id}', 'TrainersController@show');
+        Route::get('trainer/create', 'TrainersController@create');
+        Route::post('trainer/create', 'TrainersController@store');
+        Route::get('trainer/edit/{id}', 'TrainersController@edit');
+        Route::post('trainer/edit/{id}', 'TrainersController@update');
+        Route::post('trainer/delete/{id}', 'TrainersController@delete');
+    });
+
+    /*  workshop Route */
+    Route::group([
+        'prefix'    => 'workshops'
+    ],function(){
+        Route::get('/', 'EventsController@index');
+        Route::get('workshop/{id}', 'WorkshopsController@show');
+        Route::get('workshop/create', 'WorkshopsController@create');
+        Route::post('workshop/create', 'WorkshopsController@store');
+        Route::get('workshop/edit/{id}', 'WorkshopsController@edit');
+        Route::post('workshop/edit/{id}', 'WorkshopsController@update');
+        Route::post('workshop/delete/{id}', 'WorkshopsController@delete');
+    });
 });
 
 
