185a356928dc8d8e4b2d55cb8bcf81602241f2a0
Hesham\Hanafawy <hesham.hanafi911@gmail.com>
2019-10-14 00:08:59 +0200

    add centers and trainers controllers

---
+++ src/Http/Controllers/Admin/CentersController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace S3geeks\Events\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+
+class CentersController extends Controller
+{
+    //
+}
--- src/Http/Controllers/Admin/TrainersController.php
+++ src/Http/Controllers/Admin/TrainersController.php
@@ -1,8 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace S3geeks\Events\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
 
 class TrainersController extends Controller
 {
