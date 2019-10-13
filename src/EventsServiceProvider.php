df733b6e58458337d1a63a8f9296861e3d676fab
Hesham\Hanafawy <hesham.hanafi911@gmail.com>
2019-10-14 00:05:20 +0200

    publish view & seeder and add layout load

--- src/EventsServiceProvider.php
+++ src/EventsServiceProvider.php
@@ -15,14 +15,27 @@ Class EventsServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__. '/routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/views', 'viewEvents');
         $this->loadViewsFrom(__DIR__.'/views/Admin', 'adminEvents');
         $this->loadViewsFrom(__DIR__.'/views/Frontend', 'frontendEvents');
         $this->loadMigrationsFrom(__DIR__ . '/Database/migrations');
 
+
         $this->publishes([
+            __DIR__.'/views/' => resource_path('views/vendor/viewEvents'),
             __DIR__.'/views/Admin' => resource_path('views/vendor/Admin/adminEvents'),
             __DIR__.'/views/Frontend' => resource_path('views/vendor/Frontend/frontendEvents'),
         ]);
+        $this->publishes([
+            __DIR__.'/Assets' => public_path('vendor/Events'),
+        ], 'public');
+
+        $this->publishSeeds();
+    }
+
+    private function publishSeeds()
+    {
+        $this->publishes([__DIR__ . '/Database/seeds' => base_path('database/seeds')], 'seeds');
     }
 
     public function register()
