2f89ea5bd70023a9d90e83b3613e20db7290c330
Hesham\Hanafawy <hesham.hanafi911@gmail.com>
2019-11-01 02:12:44 +0200

    add index view to trainers and centers modules

--- src/views/Admin/centers/index.blade.php
+++ src/views/Admin/centers/index.blade.php
@@ -1,11 +1,46 @@
-<!DOCTYPE html>
-<html>
-<head>
+@extends('viewEvents::layouts.app')
 
-</head>
-<body>
-<div>
-    <h1> index view from admin in package </h1>
-</div>
-</body>
-</html>
+
+@section('titlePage')
+    <h1>centers index</h1>
+@endsection
+@section('content')
+    <div>
+        <table class="table table-dark">
+            <thead>
+            <tr>
+                <th scope="col">num</th>
+                <th scope="col">اسم المكان</th>
+                <th scope="col">العنوان</th>
+                <th scope="col">رقم التليفون</th>
+                <th scope="col">الأيميل</th>
+                <th scope="col">رابط الفيسبوك</th>
+                <th scope="col">عدد مقاعد</th>
+                <th scope="col">المكان مكيف</th>
+                <th scope="col">تعديل</th>
+                <th scope="col">حذف</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($items as $item)
+            <tr>
+                <th scope="row">{{$item->id}}</th>
+                <td>{{$item->title}}</td>
+                <td>{{$item->address}}</td>
+                <td>{{$item->num_phone}}</td>
+                <td>{{$item->email}}</td>
+                <td><a href="{{$item->url_fb}}" target="_blank">facebook</a></td>
+                <td>{{$item->num_class_room}}</td>
+                <td>{{$item->Air_conditioned_place == 1 ? 'متاح' : 'غير متاح'}}</td>
+                <td><a href="{{route('center-edit',['id'=>$item->id])}}" class="btn btn-info">edit</a></td>
+                <td><a href="{{route('center-delete',['id'=>$item->id])}}" class="btn btn-danger">delete</a></td>
+            </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
+
+
+@section('customScript')
+@endsection
--- src/views/Admin/trainers/index.blade.php
+++ src/views/Admin/trainers/index.blade.php
@@ -1,11 +1,47 @@
-<!DOCTYPE html>
-<html>
-<head>
+@extends('viewEvents::layouts.app')
 
-</head>
-<body>
-<div>
-    <h1> index view from admin in package </h1>
-</div>
-</body>
-</html>
+
+@section('titlePage')
+    <h1>Events index</h1>
+@endsection
+@section('content')
+    <div>
+        <table class="table table-dark">
+            <thead>
+            <tr>
+                <th scope="col">num</th>
+                <th scope="col">اسم المدرب</th>
+                <th scope="col">العنوان</th>
+                <th scope="col">الإيميل</th>
+                <th scope="col">رقم التليفون</th>
+                <th scope="col">رقم التليفون العمل</th>
+                <th scope="col">رابط الفيس بوك</th>
+                <th scope="col">المجال</th>
+                <th scope="col">تعديل</th>
+                <th scope="col">حذف</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($items as $item)
+                <tr>
+                    <th scope="row">{{$item->id}}</th>
+                    <td>{{$item->first_name.' '.$item->last_name}}</td>
+                    <td>{{$item->address}}</td>
+                    <td>{{$item->email}}</td>
+                    <td>{{$item->num_phone}}</td>
+                    <td>{{$item->work_num_phone}}</td>
+                    <td><a href="{{$item->url_fb}}" target="_blank">facebook</a></td>
+                    <td>{{$item->field}}</td>
+                    <td><a href="{{route('trainer-edit',['id'=>$item->id])}}" class="btn btn-info">edit</a></td>
+                    <td><a href="{{route('trainer-delete',['id'=>$item->id])}}" class="btn btn-danger">delete</a></td>
+                </tr>
+            @endforeach
+
+            </tbody>
+        </table>
+    </div>
+@endsection
+
+
+@section('customScript')
+@endsection
